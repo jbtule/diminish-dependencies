2cd31fd18cacd48e8516e6e4ac6a81c198147cf3
Jay Tuley <jay+github@tuley.name>
2012-10-30 22:56:59 -0500

    Working on mono

--- DiminishDependencies.Diminished/Diminish/src/Setup.cs
+++ DiminishDependencies.Diminished/Diminish/src/Setup.cs
@@ -11,28 +11,43 @@ namespace DiminishDependencies.Diminished.Diminish
     public static class Setup
     {
         static private readonly IDictionary<string, Assembly> _loaded =  new Dictionary<string, Assembly>();
-        static public  Assembly AssemblyLoad(string asssemblyName)
-        {
-            var shortName = new AssemblyName(asssemblyName).Name;
-            if (!_loaded.ContainsKey(shortName))
-            {
-                var resourceName = String.Format("DiminishDependencies.Diminished.Diminish.refs.{0}.dep-lzma", shortName);
-                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
-                {
-        
-                    if (stream == null)
-                        throw new Exception(string.Format(
-                            "Missing embedded dependency {0} was looking for resource {1}", asssemblyName, resourceName));
-                    using (var memStream = new MemoryStream())
-                    {
-                        Zipper.Decode(stream, memStream);
-                        var assembly = Assembly.Load(memStream.ToArray());
-                        _loaded.Add(shortName, assembly);
-                    }
-                }
-            }
-            return _loaded[shortName];
-        }
+		static public  Assembly AssemblyLoad(string asssemblyName)
+		{
+			var shortName = new AssemblyName(asssemblyName).Name;
+			if (!_loaded.ContainsKey(shortName))
+			{
+				Func<string,string> resourceFormat = ext => String.Format("DiminishDependencies.Diminished.Diminish.refs.{0}.{1}.dep-lzma", shortName, ext);
+				
+				var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream( resourceFormat("dll"))
+					??  Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceFormat("exe"));
+				if (stream == null)
+					throw new Exception(string.Format(
+						"Missing embedded dependency {0} was looking for resource {1} or {2}", asssemblyName, resourceFormat("dll"),resourceFormat("exe")));
+				using (stream)
+				{
+					var pdb =Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceFormat("pdb"));
+					byte[] symbols =null;
+					
+					if(pdb !=null)
+					{
+						using(pdb)
+							using (var memStream = new MemoryStream())
+						{
+							Zipper.Decode(stream, memStream);
+							symbols = memStream.ToArray();
+						}
+					}
+					
+					using (var memStream = new MemoryStream())
+					{
+						Zipper.Decode(stream, memStream);
+						var assembly = symbols == null ? Assembly.Load(memStream.ToArray()) : Assembly.Load(memStream.ToArray(), symbols);
+						_loaded.Add(shortName, assembly);
+					}
+				}
+			}
+			return _loaded[shortName];
+		}
 
         public static void Resolver()
         {
