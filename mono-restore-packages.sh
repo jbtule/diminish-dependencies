9bc27fdaf17eb5010bcf902e2037cafabf0a7fc4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-24 23:20:05 +0800

    Delete mono-restore-packages.sh

--- mono-restore-packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir packages
-cd packages
-mono --runtime=v4.0 ../.nuget/NuGet.exe install ../.nuget/packages.config
-mono --runtime=v4.0 ../.nuget/NuGet.exe install ../DiminishDependencies/packages.config
-mono --runtime=v4.0 ../.nuget/NuGet.exe install ../DiminishDependencies.Diminished/packages.config
